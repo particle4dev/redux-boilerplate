1f3ededb3160a68ab246ccfd4e1b5ccceab437f1
hoang <particle4dev@gmail.com>
2016-11-20 10:47:28 +0700

    [client] [src] use HashHistory on cordova

--- app/app.js
+++ app/app.js
@@ -15,7 +15,7 @@ import 'file?name=[name].[ext]!./.htaccess';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { applyRouterMiddleware, Router, browserHistory } from 'react-router';
+import { applyRouterMiddleware, Router, hashHistory, browserHistory } from 'react-router';
 import { syncHistoryWithStore } from 'react-router-redux';
 import { useScroll } from 'react-router-scroll';
 import LanguageProvider from 'containers/LanguageProvider';
@@ -32,13 +32,14 @@ import 'sanitize.css/sanitize.css';
 // Optionally, this could be changed to leverage a created history
 // e.g. `const browserHistory = useRouterHistory(createBrowserHistory)();`
 const initialState = {};
-const store = configureStore(initialState, browserHistory);
+const historyDrive = process.env.NODE_ENV === 'cordova' ? hashHistory : browserHistory;
+const store = configureStore(initialState, historyDrive);
 
 // Sync history and store, as the react-router-redux reducer
 // is under the non-default key ("routing"), selectLocationState
 // must be provided for resolving how to retrieve the "route" in the state
 import { selectLocationState } from 'containers/App/selectors';
-const history = syncHistoryWithStore(browserHistory, store, {
+const history = syncHistoryWithStore(historyDrive, store, {
   selectLocationState: selectLocationState(),
 });
 
